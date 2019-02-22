8d8aa2b4f0164fb2ebc2503bbbbc5be3280de897
RMH <rory.hennessey@gmail.com>
2019-02-22 12:16:49 -0500

    cards are showing up. proptypes added.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
         {props.cards.map((card, index) => {
-          <Card key={index} 
+          return <Card key={index} 
             headline={card.headline}
             img={card.img}
             author={card.author}
@@ -19,5 +18,10 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
+
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object)
+};
+
 
 export default Cards;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 
 const Tabs = props => {
   return (
@@ -15,4 +16,9 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+}
+
+
 export default Tabs;
